340375c8513c9a5ba2d90dc609bf14997fc927e0
SilverPrize <silverprizelee@gmail.com>
2018-05-02 18:03:08 +0900

    fix syntax

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('./sw.js').then(registration {
+      navigator.serviceWorker.register('./sw.js').then(registration => {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }).catch(function(err) {
